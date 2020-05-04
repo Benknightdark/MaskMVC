e93e57138463f3b74bc86c82a9492fc8b00e74ee
moon791017 <moon791017@gmail.com>
2020-05-04 14:56:32 +0800

    O

--- wwwroot/js/CustomMask.js
+++ wwwroot/js/CustomMask.js
@@ -1,21 +1,18 @@
 
-export const initMaskMobilePhone = () => {
-    const maskMobilePhoneEl = document.querySelectorAll('.mask-mobile-phone');
-    for (let index = 0; index < maskMobilePhoneEl.length; index++) {
-        const element = maskMobilePhoneEl[index];
-        const mask = IMask(
-            element,
-            {
-                mask: '{\\0}{9}(00)-(000)-(000)',
-                lazy: false,
-                overwrite: true,
-                autofix: true,
-            });
-        mask.on("accept", () => {
-           // element.value=mask.unmaskedValue;
-            mask.updateValue(mask.unmaskedValue)
-            element.value=mask.unmaskedValue;
-            console.log(mask.unmaskedValue)
+export const initMaskMobilePhone = (element) => {
+    const queryElement= $(element)
+    queryElement.data('unmask-value', '')
+    const mask = IMask(
+        element,
+        {
+            mask: '{\\0}{9}(00)-(000)-(000)',
+            lazy: false,
+            overwrite: true,
+            autofix: true,
         });
-    }
+    mask.on("accept", () => {
+        queryElement.data('unmask-value', mask.unmaskedValue)
+        console.log(queryElement.data('unmask-value'))
+        
+    });
 }
--- wwwroot/js/InitMaskInput.js
+++ wwwroot/js/InitMaskInput.js
@@ -1,6 +1,21 @@
-import {initMaskMobilePhone} from './CustomMask.js';
+import { initMaskMobilePhone } from './CustomMask.js';
 
 ((document, window) => {
-    initMaskMobilePhone();
-    
+    // 初始化手機號碼Mask
+    const maskMobilePhoneEl = document.querySelectorAll('.mask-mobile-phone');
+    // maskMobilePhoneEl.setAttribute('data-unmask-value','')
+    for (let index = 0; index < maskMobilePhoneEl.length; index++) {
+        const element = maskMobilePhoneEl[index];
+        initMaskMobilePhone(element);
+    }
+    $('form').submit(function () {
+        event.preventDefault();
+        console.log()
+        const maskInputEl = $('.mask-input');
+        for (let index = 0; index < maskInputEl.length; index++) {
+            const element = maskInputEl[index];
+            $(element).val($(element).data('unmask-value'));
+        }
+    });
+
 })(document, window);
